3a938f3b847ce88ea161942c5566d53dfe42f1fd
ristespasov <ristespasov94@gmail.com>
2019-09-23 17:04:32 +0200

    current weather api comp added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,23 @@ class App extends Component {
     icon: undefined,
     city: undefined,
     temperature: undefined,
+    realFeel: undefined,
     description: undefined,
+    windSpeed: undefined,
+    humidity: undefined,
+    pressure: undefined,
+    uv: undefined,
+    partOfDay: undefined,
+    windDirectionTxt: undefined,
+    windDirectionDeg: undefined,
+    dewPoint: undefined,
+    clouds: undefined,
+    visibility: undefined,
+    solarRadiation: undefined,
+    rain: undefined,
+    snow: undefined,
+    sunrise: undefined,
+    sunset: undefined,
     error: undefined
   }
   getWeather = async (e) => {
@@ -25,11 +41,23 @@ class App extends Component {
         icon: data.data[0].weather.icon,
         city: data.data[0].city_name,
         temperature: data.data[0].temp,
+        realFeel: data.data[0].app_temp,
         description: data.data[0].weather.description,
         windSpeed: data.data[0].wind_spd,
         humidity: data.data[0].rh,
         pressure: data.data[0].pres,
         uv: data.data[0].uv,
+        partOfDay: data.data[0].pod,
+        windDirectionTxt: data.data[0].wind_cdir,
+        windDirectionDeg: data.data[0].wind_dir,
+        dewPoint: data.data[0].dewpt,
+        clouds: data.data[0].clouds,
+        visibility: data.data[0].vis,
+        solarRadiation: data.data[0].solar_rad,
+        rain: data.data[0].precip,
+        snow: data.data[0].snow,
+        sunrise: data.data[0].sunrise,
+        sunset: data.data[0].sunset,
         error: ""
       });
     } else {
@@ -38,10 +66,22 @@ class App extends Component {
         city: undefined,
         temperature: undefined,
         description: undefined,
+        realFeel: undefined,
         windSpeed: undefined,
         humidity: undefined,
         pressure: undefined,
         uv: undefined,
+        partOfDay: undefined,
+        windDirectionTxt: undefined,
+        windDirectionDeg: undefined,
+        dewPoint: undefined,
+        clouds: undefined,
+        visibility: undefined,
+        solarRadiation: undefined,
+        rain: undefined,
+        snow: undefined,
+        sunrise: undefined,
+        sunset: undefined,
         error: "Please enter the values."
       });
     }
@@ -59,6 +99,7 @@ class App extends Component {
                     icon={this.state.icon}
                     city={this.state.city}
                     temperature={this.state.temperature}
+                    realFeel={this.state.realFeel}
                     description={this.state.description}
                     error={this.state.error}
                   />
@@ -69,6 +110,21 @@ class App extends Component {
                     humidity={this.state.humidity}
                     pressure={this.state.pressure}
                     uv={this.state.uv}
+                  />
+                </div>
+                <div>
+                  <Weather
+                    partOfDay={this.state.partOfDay}
+                    windDirectionTxt={this.state.windDirectionTxt}
+                    windDirectionDeg={this.state.windDirectionDeg}
+                    dewPoint={this.state.dewPoint}
+                    clouds={this.state.clouds}
+                    visibility={this.state.visibility}
+                    solarRadiation={this.state.solarRadiation}
+                    rain={this.state.rain}
+                    snow={this.state.snow}
+                    sunrise={this.state.sunrise}
+                    sunset={this.state.sunset}
                   />
                 </div>
               </div>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -12,6 +12,9 @@ const Weather = props => (
             props.temperature && <p>{Math.round(props.temperature)} °C</p>
         }
         {
+            props.realFeel && <p>Real feel: {Math.round(props.realFeel)} °C</p>
+        }
+        {
             props.description && <p>{props.description}</p>
         }
         {
@@ -21,11 +24,40 @@ const Weather = props => (
             props.humidity && <p>{props.humidity}%</p>
         }
         {
-            props.pressure && <p>{Math.round(props.pressure)} mba</p>
+            props.pressure && <p>{Math.round(props.pressure)} mb</p>
         }
         {
             props.uv && <p>{Math.round(props.uv)}</p>
-
+        }
+        {
+            props.partOfDay && <p>Part of day: {props.partOfDay}</p>
+        }
+        {
+            props.windDirectionTxt && props.windDirectionDeg && <p>{props.windDirectionTxt}, {Math.round(props.windDirectionDeg)}°</p>
+        }
+        {
+            props.dewPoint && <p>{Math.round(props.dewPoint)} °C</p>
+        }
+        {
+            props.clouds && <p>{props.clouds}%</p>
+        }
+        {
+            props.visibility && <p>Visibility: {props.visibility} KM</p>
+        }
+        {
+            props.solarRadiation && <p>Solar radiation: {props.solarRadiation} W/m2</p>
+        }
+        {
+            props.rain && <p>{props.rain} mm/hr</p>
+        }
+        {
+            props.snow && <p>{props.snow} mm/hr</p>
+        }
+        {
+            props.sunrise && <p>{props.sunrise}</p>
+        }
+        {
+            props.sunset && <p>{props.sunset}</p>
         }
         {
             props.error && <p>{props.error}</p>
