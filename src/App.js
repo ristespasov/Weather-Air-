38db0d8c05f44032a314c9babccaec0e3e147909
ristespasov <ristespasov94@gmail.com>
2019-09-27 03:05:06 +0200

    weather icon statements added

--- src/App.js
+++ src/App.js
@@ -91,100 +91,6 @@ class App extends Component {
       });
     }
 
-    ///////////////// TODO ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    function getIcon(icon) {
-      switch (data.data[0].weather.description) {
-        case 'Thunderstorm with light rain':
-        case 'Thunderstorm with rain':
-        case 'Thunderstorm with heavy rain':
-          icon = <img alt="icon" src="icons/thunderstorm-and-rain.png" />;
-          break;
-        case 'Thunderstorm with light drizzle':
-        case 'Thunderstorm with drizzle':
-        case 'Thunderstorm with heavy drizzle':
-          icon = <img alt="icon" src="icons/thunderstorm.png" />
-          document.body.style.backgroundImage = 'url("images/bg-06.jpg")';
-          break;
-        case 'Thunderstorm with Hail':
-          icon = <img alt="icon" src="icons/thunderstorm-and-hail.png" />
-          document.body.style.backgroundImage = 'url("images/bg-10.jpg")';
-          break;
-        case 'Light drizzle':
-        case 'Drizzle':
-        case 'Heavy drizzle':
-          icon = <img alt="icon" src="icons/drizzle.png" />
-          document.body.style.backgroundImage = 'url("images/bg-04.jpg")';
-          break;
-        case 'Light rain':
-        case 'Moderate rain':
-        case 'Heavy rain':
-        case 'Freezing rain':
-        case 'Light shower rain':
-        case 'Shower rain':
-        case 'Heavy shower rain':
-          icon = <img alt="icon" src="icons/rain.png" />;
-          document.body.style.backgroundImage = 'url("images/bg-04.jpg")';
-          break;
-        case 'Light snow':
-        case 'Snow':
-        case 'Heavy snow':
-        case 'Snow shower':
-        case 'Heavy snow shower':
-        case 'Flurries':
-          icon = <img alt="icon" src="icons/snow.png" />
-          document.body.style.backgroundImage = 'url("images/bg-08.jpg")';
-          break;
-        case 'Sleet':
-        case 'Heavy sleet':
-          icon = <img alt="icon" src="icons/sleet.png" />
-          document.body.style.backgroundImage = 'url("images/bg-08.jpg")';
-          break;
-        case 'Mix snow/rain':
-          icon = <img alt="icon" src="icons/snow-and-rain.png" />
-          document.body.style.backgroundImage = 'url("images/bg-08.jpg")';
-          break;
-        case 'Mist':
-        case 'Smoke':
-        case 'Haze':
-        case 'Sand/dust':
-        case 'Fog':
-        case 'Freezing fog':
-          icon = <img alt="icon" src="icons/fog.png" />
-          break;
-        case 'Clear sky':
-          if (this.partOfDay === "d") {
-            icon = <img alt="icon" src="icons/clear-sky-day.png"></img>;
-            document.body.style.backgroundImage = 'url("images/bg-01.jpg")';
-          } else {
-            icon = <img alt="icon" src="icons/clear-sky-night.png" />;
-            document.body.style.backgroundImage = 'url("images/bg-12.jpg")';
-          }
-          break;
-        case 'Few clouds':
-          if (this.partOfDay === "d") {
-            icon = <img alt="icon" src="icons/few-clouds-day.png" />
-            document.body.style.backgroundImage = 'url("images/bg-02.jpg")';
-          } else {
-            icon = <img alt="icon" src="icons/few-clouds-night.png" />
-            document.body.style.backgroundImage = 'url("images/bg-09.jpg")';
-          }
-          break;
-        case 'Scattered clouds':
-          icon = <img alt="icon" src="icons/cloud.png" />
-          document.body.style.backgroundImage = 'url("images/bg-03.jpg")';
-          break;
-        case 'Broken clouds':
-        case 'Overcast clouds':
-          icon = <img alt="icon" src="icons/clouds.png"></img>;
-          document.body.style.backgroundImage = 'url("images/bg-03.jpg")';
-          break;
-        default:
-          break;
-      }
-    }
-    getIcon();
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
     let counter = document.getElementById('counter').getContext('2d');
     let no = 0;
     let pointToFill = 4.72;
@@ -193,41 +99,41 @@ class App extends Component {
     let diff;
 
     function fillCounter() {
+
       counter.textAlign = 'center';
-
       counter.font = '25px Quicksand, sans-serif';
-
       counter.clearRect(0, 0, cw, ch);
-
       counter.lineWidth = 15;
-
       counter.fillStyle = '#fff';
 
       diff = ((no / 100) * Math.PI * 2 * 10);
 
-      if (no <= 50) {
+      if (no > 0 && no <= 50) {
         counter.strokeStyle = 'rgba(0, 255, 0, 0.5)';
+        counter.fillText(no, 100, 75);
+        counter.fillText('/100 AQI', 100, 110);
         counter.fillText('Good', 100, 145);
       } else if (no >= 51 && no <= 100) {
         counter.strokeStyle = 'rgba(255, 255, 0, 0.5)';
+        counter.fillText(no, 100, 75);
+        counter.fillText('/100 AQI', 100, 110);
         counter.fillText('Moderate', 100, 145);
       } else if (no >= 101) {
         counter.strokeStyle = 'rgba(255, 0, 0, 0.5)';
+        counter.fillText(no, 100, 75);
+        counter.fillText('/100 AQI', 100, 110);
         counter.fillText('Poor', 100, 145);
+      } else if (no === 0) {
+        counter.fillText('N/A', 100, 90);
+        counter.fillText('/100 AQI', 100, 125);
       }
 
-      counter.fillText(no, 100, 75);
-      counter.fillText('/100 AQI', 100, 110);
-
-
       counter.beginPath();
-
-      counter.arc(100, 100, 90, pointToFill, diff / 10 + pointToFill);   //arc(x,y,radius,start,stop)
-
-      counter.stroke();   // to fill stroke
+      counter.arc(100, 100, 90, pointToFill, diff / 10 + pointToFill);
+      counter.stroke();
 
       if (no === data.data[0].aqi) {
-        clearTimeout(fill);   //fill is a variable that call the function fillcounter()
+        clearTimeout(fill);
       }
       no++;
     }
--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,9 +1,151 @@
 import React from 'react';
+import thunderstormRain from '../icons/thunderstorm-rain.png';
+import thunderstormDrizzleDay from '../icons/thunderstorm-drizzle-d.png';
+import thunderstormDrizzleNight from '../icons/thunderstorm-drizzle-n.png';
+import hailDay from '../icons/hail-d.png';
+import hailNight from '../icons/hail-n.png';
+import drizzle from '../icons/drizzle.png';
+import rain from '../icons/rain.png';
+import showerRainDay from '../icons/shower-rain-d.png';
+import showerRainNight from '../icons/shower-rain-n.png';
+import snow from '../icons/snow.png';
+import rainSnow from '../icons/rain-snow.png';
+import sleet from '../icons/sleet.png';
+import fogDay from '../icons/fog-d.png';
+import fogNight from '../icons/fog-n.png';
+import clearSkyDay from '../icons/clear-sky-d.png';
+import clearSkyNight from '../icons/clear-sky-n.png';
+import fewCloudsDay from '../icons/few-clouds-d.png';
+import fewCloudsNight from '../icons/few-clouds-n.png';
+import clouds from '../icons/clouds.png';
+import unknown from '../icons/unknown.png';
 
 const Icon = props => (
     <div>
         {
-            props.icon != null && <div className="current-weather-icon">{props.getIcon}</div>
+            props.icon != null &&
+            <div>
+                <span>
+                    {
+                        props.icon === ('t01d' || 't01n' || 't02d' || 't02n' || 't03d' || 't03n') &&
+                        <img className="current-weather-icon" alt="icon" src={thunderstormRain} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === 't04d' &&
+                        <img className="current-weather-icon" alt="icon" src={thunderstormDrizzleDay} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === 't04n' &&
+                        <img className="current-weather-icon" alt="icon" src={thunderstormDrizzleNight} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === 't05d' &&
+                        <img className="current-weather-icon" alt="icon" src={hailDay} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === 't05n' &&
+                        <img className="current-weather-icon" alt="icon" src={hailNight} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === ('d01d' && 'd01n' && 'd02d' && 'd02n' && 'd03d' && 'd03n') &&
+                        <img className="current-weather-icon" alt="icon" src={drizzle} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === ('r01d' && 'r01n' && 'r02d' && 'r02n' && 'r03d' && 'r03n' && 'f01d' && 'f01n' && 'r04d' && 'r04n') &&
+                        <img className="current-weather-icon" alt="icon" src={rain} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === ('r04d' && 'r05d' && 'r06d') &&
+                        <img className="current-weather-icon" alt="icon" src={showerRainDay} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === ('r04n' && 'r05n' && 'r06n') &&
+                        <img className="current-weather-icon" alt="icon" src={showerRainNight} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === ('s01d' && 's01n' && 's02d' && 's02n' && 's03d' && 's03n' && 's06d' && 's06n') &&
+                        <img className="current-weather-icon" alt="icon" src={snow} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === ('s04d' && 's04n') &&
+                        <img className="current-weather-icon" alt="icon" src={rainSnow} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === ('s05d' && 's045n') &&
+                        <img className="current-weather-icon" alt="icon" src={sleet} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === ('a01d' && 'a02d' && 'a03d' && 'a04d' && 'a05d' && 'a06d') &&
+                        <img className="current-weather-icon" alt="icon" src={fogDay} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === ('a01n' && 'a02n' && 'a03n' && 'a04n' && 'a05n' && 'a06n') &&
+                        <img className="current-weather-icon" alt="icon" src={fogNight} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === 'c01d' &&
+                        <img className="current-weather-icon" alt="icon" src={clearSkyDay} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === 'c01n' &&
+                        <img className="current-weather-icon" alt="icon" src={clearSkyNight} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === 'c02d' &&
+                        <img className="current-weather-icon" alt="icon" src={fewCloudsDay} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === 'c02n' &&
+                        <img className="current-weather-icon" alt="icon" src={fewCloudsNight} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === ('c03d' && 'c03n' && 'c04d' && 'c04n') &&
+                        <img className="current-weather-icon" alt="icon" src={clouds} />
+                    }
+                </span>
+                <span>
+                    {
+                        props.icon === ('u00d' || 'u00n') &&
+                        <img className="current-weather-icon" alt="icon" src={unknown} />
+                    }
+                </span>
+            </div>
         }
     </div>
 )
