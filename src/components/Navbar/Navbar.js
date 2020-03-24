ba168f4aa875e7ab5d2810e6784f7edc5c63dd74
ristespasov <ristespasov94@gmail.com>
2020-03-24 22:04:04 +0100

    Navbar component updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,19 +4,20 @@ import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import logoNav from '../../logo/logo-nav.png';
 
-const navbar = () => (
-
-    <Navbar className="navbar" expand="md">
-        <Navbar.Brand href="#home"><img className="logo-nav" alt="logo" src={logoNav} /></Navbar.Brand>
-        <Navbar.Toggle className="toggler" aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <NavLink to="/" activeClassName="active-link" className="nav-link" style={{ textDecoration: 'none' }} exact><span className="a">Home</span></NavLink>
-                <NavLink to="weather-and-air" activeClassName="active-link" className="nav-link" style={{ textDecoration: 'none' }}><span className="a">Weather and Air</span></NavLink>
-                <NavLink to="blog" activeClassName="active-link" className="nav-link" style={{ textDecoration: 'none' }}><span className="a">Blog</span></NavLink>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
-)
+const navbar = () => {
+    return (
+        <Navbar collapseOnSelect className="navbar" expand="md">
+            <Navbar.Brand href="#home"><img className="logo-nav" alt="logo" src={logoNav} /></Navbar.Brand>
+            <Navbar.Toggle className="toggler" aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse>
+                <Nav className="mr-auto">
+                    <NavLink to="/" activeClassName="active-link" className="nav-link" exact><span>Home</span></NavLink>
+                    <NavLink to="weather-and-air" activeClassName="active-link" className="nav-link"><span >Weather and Air</span></NavLink>
+                    <NavLink to="blog" activeClassName="active-link" className="nav-link"><span>Blog</span></NavLink>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
 
 export default navbar;
